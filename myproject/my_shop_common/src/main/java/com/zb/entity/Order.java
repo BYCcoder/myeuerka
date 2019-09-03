4a8fb52f529847d50057655525c2b13af0048f3b
BYCcoder <bianyichang@126.com>
2019-09-03 11:46:47 +0800

    local update file

--- myproject/my_shop_common/src/main/java/com/zb/entity/Order.java
+++ myproject/my_shop_common/src/main/java/com/zb/entity/Order.java
@@ -3,6 +3,7 @@ package com.zb.entity;
 import java.io.Serializable;
 
 public class Order implements Serializable {
+    //新开发的功能
     private Integer id;
     private String name;
     private Integer price;
